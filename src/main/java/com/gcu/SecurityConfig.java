00fc39ab5e5c632b0a995815a221bd447fde4fee
baustin91 <baustin14@my.gcu.edu>
2023-10-11 22:06:52 -0700

    Updated logout

--- src/main/java/com/gcu/SecurityConfig.java
+++ src/main/java/com/gcu/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 			)
 			
 			.logout((logout) -> logout
-	                .logoutUrl("/logout")
+	                .logoutUrl("/")
 	                .logoutSuccessUrl("/")
 		            .invalidateHttpSession(true)
 		            .clearAuthentication(true)
